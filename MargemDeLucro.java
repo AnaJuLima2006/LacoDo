564cfb69c61179be2ade0e33da4ac13bbde19b83
anajuprogrammer06 <105178561+AnaJuLima2006@users.noreply.github.com>
2022-11-19 17:06:18 -0300

    Add files via upload

---
+++ MargemDeLucro.java
@@ -0,0 +1,31 @@
+package lacoRepeticao;
+import java.util.Scanner;
+public class margemDeLucro {
+
+	public static void main(String[] args) {
+		
+		Scanner leitura = new Scanner(System.in);
+	
+				int resposta;
+				double preco, lucro, venda;
+
+		        do{
+					
+					
+		            System.out.println("Digite o valor do produto:");
+		            preco = leitura.nextDouble();
+
+		            System.out.println("Digite a margem de lucro do produto em %:");
+		            lucro = leitura.nextDouble();
+
+		            venda = ((lucro / 100) * preco) + preco;
+		            System.out.println("O produto foi vendido por: R$" + venda);
+
+		            System.out.println("Deseja continuar a execução? S - 1, N -2");
+		            resposta = leitura.nextInt();
+		        }while (resposta!=2);
+      }
+	
+}
+
+
